e424f2ac2eb23e8c2f89b4fb078a6574092d278f
Biagio Arobba <biagio@ch-labs.com>
2020-02-04 02:38:09 -0600

    Mass update

--- your_app/cli_binding.py
+++ your_app/cli_binding.py
@@ -1,10 +1,24 @@
 
+from your_app_webapi_client.BankDirectory import BankDirectory
+
+
 def directory_list(command):
-    return None
+
+    directory = BankDirectory('http://localhost:5000')
+
+    account_list = directory.read_list()
+
+    for account in account_list:
+        print(account.__dict__)
 
 
 def directory_get(command):
-    return None
+
+    directory = BankDirectory('http://localhost:5000')
+
+    account = directory.read(command.account_id)
+
+    print(account.__dict__)
 
 
 def account_credit(command):
